5d1024e9950ee4ee51d3f5304aed4467d7361e1a
Dykky <dikyhabib13@gmail.com>
2025-03-15 20:57:10 +0700

    first commit

--- Codelab/Modul_2/Codelab 2/src/Main.java
+++ Codelab/Modul_2/Codelab 2/src/Main.java
@@ -1,28 +1,28 @@
 public class Main {
     public static void main(String[] args){
-        RekeningBank rekening1 = new RekeningBank();
-        RekeningBank rekening2 = new RekeningBank();
+        RekeningBank rekening1 = new RekeningBank(); //pembuatan objek untuk RekeningBank
+        RekeningBank rekening2 = new RekeningBank(); //pembuatan objek untuk RekeningBank
 
-        rekening1.nomorRekening = "202410370110208";
-        rekening2.nomorRekening = "202410370110218";
+        rekening1.nomorRekening = "202410370110208"; //Atribut rekening1
+        rekening2.nomorRekening = "202410370110218"; //Atribut rekening2
 
-        rekening1.namaPemilik = "Dicky Habib Putra Hidayatullah";
-        rekening2.namaPemilik = "Revansa adha dwi yuliarto";
+        rekening1.namaPemilik = "Dicky Habib Putra Hidayatullah"; //Atribut rekening1
+        rekening2.namaPemilik = "Revansa adha dwi yuliarto"; //Atribut rekening2
 
-        rekening1.saldo = (1000000);
-        rekening2.saldo = (900000);
+        rekening1.saldo = (1000000); //Atribut rekening1
+        rekening2.saldo = (900000); //Atribut rekening2
 
-        rekening1.tampilkaninfo();
-        rekening2.tampilkaninfo();
+        rekening1.tampilkaninfo(); //memanggil method tampilkaninfo
+        rekening2.tampilkaninfo(); //memanggil method tampilkaninfo
 
-        rekening1.setorUang(1000000);
-        rekening2.setorUang(1000000);
+        rekening1.setorUang(1000000); //memanggil method setoruang
+        rekening2.setorUang(1000000); //memanggil method setoruang
 
-        rekening1.tarikUang(100000);
-        rekening2.tarikUang(2000000);
-        System.out.println();
+        rekening1.tarikUang(100000); //memanggil method tarikuang
+        rekening2.tarikUang(2000000); //memanggil method tarikuang
+        System.out.println(); //untuk jarak antara tarikuang dengan tampilkaninfo
 
-        rekening1.tampilkaninfo();
-        rekening2.tampilkaninfo();
+        rekening1.tampilkaninfo(); //memanggil method tampilkaninfo
+        rekening2.tampilkaninfo(); //memanggil method tampilkaninfo
     }
 }
--- Codelab/Modul_2/Codelab 2/src/RekeningBank.java
+++ Codelab/Modul_2/Codelab 2/src/RekeningBank.java
@@ -1,23 +1,26 @@
 import java.sql.SQLOutput;
 
 public class RekeningBank {
-    String nomorRekening;
-    String namaPemilik;
-    double saldo;
+    String nomorRekening; //atribut untuk menyimpan nomor rekening
+    String namaPemilik; //atribut untuk menyimpan nama pemilik
+    double saldo; //atribut untuk menyimpan saldo
 
+    //method untuk menampilkan info
     void tampilkaninfo(){
         System.out.println("Nomor rekening :" + nomorRekening + "\nNama pemilik   : " + namaPemilik +"\nSaldo          : Rp "+ saldo);
         System.out.println();
     }
 
+    //method untuk menyetorkan uang ke rekening
     void setorUang(double jumlah){
         saldo += jumlah;
         System.out.println(namaPemilik + " Menyetor Rp " + jumlah + " Saldo sekarang : Rp " + saldo);
         System.out.println();
     }
 
+    //method untuk menarik uang dari rekening
     void tarikUang(double jumlah) {
-        if (jumlah > saldo) {
+        if (jumlah > saldo) { //mengecek apakah saldo bisa ditarik atau tidak
             System.out.println(namaPemilik + " Menarik Rp :" + jumlah + " .(Gagal, Saldo tidak mencukupi) Saldo saat ini : Rp " + saldo);
         } else {
             saldo -= jumlah;
