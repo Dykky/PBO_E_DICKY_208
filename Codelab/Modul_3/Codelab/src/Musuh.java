1115aa76f2be3f4919371410a875934296db1ea1
Dykky <dikyhabib13@gmail.com>
2025-03-22 22:34:53 +0700

    Firts Commit - Codelab M3

--- Codelab/Modul_3/Codelab/src/Musuh.java
+++ Codelab/Modul_3/Codelab/src/Musuh.java
@@ -1,9 +1,9 @@
-class Musuh extends karakterGame {
-    public Musuh(String nama, int kesehatan) {
+class Musuh extends karakterGame { //merupakan subclass dari karakterGame
+    public Musuh(String nama, int kesehatan) {//constuctor
         super(nama, kesehatan);
     }
 
-    @Override
+    @Override //Override method serang untuk memberikan serangan
     public void serang(karakterGame target){
         System.out.println(getnama() + " menyerang " + target.getnama() + " Menggunakan Snake Bite!");
         target.setkesehatan(target.getkesehatan() - 15);
--- Codelab/Modul_3/Codelab/src/Pahlawan.java
+++ Codelab/Modul_3/Codelab/src/Pahlawan.java
@@ -1,9 +1,9 @@
-class Pahlawan extends karakterGame {
-    public Pahlawan(String nama, int kesehatan) {
+class Pahlawan extends karakterGame { //merupakan subclass dari
+    public Pahlawan(String nama, int kesehatan) { //constuctor
         super(nama, kesehatan);
     }
 
-    @Override
+    @Override //Override method serang untuk memberikan serangan
     public void serang(karakterGame target){
         System.out.println(getnama() + " Menyerang " + target.getnama() + " menggunakan Orbital Strike!");
         target.setkesehatan(target.getkesehatan() -20 );
