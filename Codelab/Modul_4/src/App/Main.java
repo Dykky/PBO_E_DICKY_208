9d5c1320c42580477130c3e94875f79a193a0d2a
Dykky <dikyhabib13@gmail.com>
2025-05-04 19:50:10 +0700

    pembuatan class main

--- Codelab/Modul_4/src/App/Main.java
+++ Codelab/Modul_4/src/App/Main.java
@@ -5,6 +5,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Buku buku1 = new NonFiksi();
+        Buku buku2 = new Fiksi();
+        buku1.DisplayInfo();
+        buku2.DisplayInfo();
 
+        Anggota aku = new Anggota( " Dicky Habib Putra H", "E208");
+        Anggota teman = new Anggota("Iqodhul Himam ", "E179");
+
+        aku.pinjamBuku(buku1.getJudul());
+        teman.pinjamBuku(buku2.getJudul());
+        System.out.println();
+
+        aku.kembalikanBuku(buku1.getJudul());
+        teman.kembalikanBuku(buku2.getJudul());
     }
 }
