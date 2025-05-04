7f51b4c354e1cde51a18710c5267be8ad49ff409
Dykky <dikyhabib13@gmail.com>
2025-05-04 19:29:51 +0700

    pembuatan class anggota dan Peminjaman

--- Codelab/Modul_4/src/Perpustakaan/Anggota.java
+++ Codelab/Modul_4/src/Perpustakaan/Anggota.java
@@ -1,4 +1,25 @@
 package Perpustakaan;
 
-public class Anggota {
+public class Anggota implements Peminjaman {
+    String Nama;
+    String IdAnggota;
+
+    public Anggota(String nama, String idAnggota) {
+        this.Nama = nama;
+        this.IdAnggota = idAnggota;
+
+        System.out.println(" Anggota = " + nama + " (ID = " + IdAnggota + ")");
+    }
+    @Override
+    public void pinjamBuku(String judul) {
+        System.out.println("Buku " + judul + " dipinjam oleh " + Nama);
+    }
+
+    public void pinjamBuku(String judul, int durasi){
+        System.out.println("Buku " + judul + " dipinjam selama " + durasi + "hari, oleh " + Nama );
+    }
+
+    public void kembalikanBuku(String judul) {
+        System.out.println("Buku " + judul + " dikembalikan oleh " + Nama);
+    }
 }
